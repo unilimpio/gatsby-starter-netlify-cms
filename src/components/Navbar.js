544ed1e9095cd1563e79296e3e8420615527780a
Marcel Filbig <mfilbig@unilimpio.com>
2021-12-15 13:20:50 -0500

    prueba de carga de nueva branch via git gatsby v4

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+
 import logo from "../img/logo.svg";
 
 const Navbar = class extends React.Component {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Unilimpio" style={{ width: "14em", height: "10em" }}/>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -79,18 +79,7 @@ const Navbar = class extends React.Component {
                 Form Examples
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+
           </div>
         </div>
       </nav>
---
+++ src/pages/blog/2021-12-14-blog-entry.md
@@ -0,0 +1,14 @@
+---
+templateKey: blog-post
+title: blog entry
+date: 2021-12-14T16:22:46.762Z
+description: dsfdafasdf
+featuredpost: true
+featuredimage: /img/indonesia-toxic-children-pollution.jpg
+tags:
+  - blog
+  - latest
+  - news
+  - novedades
+---
+this is a blog entry, with ***rich format text***
